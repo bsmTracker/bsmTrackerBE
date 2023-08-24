4887ae000536bf942831b71ad0d094462969cf53
DONGHAEY <dongheaey@gmail.com>
2023-08-24 11:07:00 +0900

    FIX :: CORS

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: {
       origin: '*',
-      // credentials: true,
+      credentials: true,
     },
   });
   app.setGlobalPrefix('api');
