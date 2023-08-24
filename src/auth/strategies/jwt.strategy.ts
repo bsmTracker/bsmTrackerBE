696163de79293b91777fec1af6437b9a3e9fa127
DONGHAEY <dongheaey@gmail.com>
2023-08-24 14:38:08 +0900

    FIXING

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -11,7 +11,7 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
     super({
       jwtFromRequest: ExtractJwt.fromExtractors([
         (request: Request) => {
-          console.log(request?.headers['access_token'], '?????????');
+          console.log(request?.headers);
           return request?.headers['access_token'];
         },
       ]),
